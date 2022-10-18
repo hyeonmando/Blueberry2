2898238e9eb42978f9379ce5a351c96c56636e8c
hyeonmando <jeong010923@naver.com>
2022-10-18 13:35:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/blueberry2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.blueberry2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.example.blueberry2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
